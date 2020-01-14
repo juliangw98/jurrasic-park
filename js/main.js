679a35d2942a6b870251632916364164c41a4a33
julian <julian.gio.wieckardt@gmail.com>
2020-01-14 15:00:48 +0100

    go

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+//switch the photos
+  $('#pills-island-tab').click(function(){
+    // alert('clik');
+  document.getElementById("console-img").style.backgroundImage = 'url("img/man.jpg")';
+
+  });
+  $('#pills-movie-tab').click(function(){
+    // alert('clik');
+  document.getElementById("console-img").style.backgroundImage = 'url("img/kanye.jpg")';
+
+  });
+  $('#pills-hotel-tab').click(function(){
+    // alert('clik');
+  document.getElementById("console-img").style.backgroundImage = 'url("img/man.jpg")';
+
+  });
+  $('#pills-dining-tab').click(function(){
+    // alert('clik');
+  document.getElementById("console-img").style.backgroundImage = 'url("img/kanye.jpg")';
+
+  });
+  $('#pills-activities-tab').click(function(){
+    // alert('clik');
+  document.getElementById("console-img").style.backgroundImage = 'url("img/man.jpg")';
+
+  });
+
+
+
+
+
+
+
+
+});
