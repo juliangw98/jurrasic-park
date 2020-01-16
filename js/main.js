10454348998e06a289cc5a18bd84ef7e57ccd331
julian <julian.gio.wieckardt@gmail.com>
2020-01-16 22:33:11 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,49 @@
 $(document).ready(function() {
 
-//switch the photos
+// Dinosaurus slider
+  var swiper = new Swiper('.swiper-container', {
+      navigation: {
+          nextEl: '.button-next',
+          prevEl: '.button-prev',
+      },
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop:true,
+      centeredSlides: true,
+      pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+      },
+  });
+
+
+//console images switch
   $('#pills-island-tab').click(function(){
     // alert('clik');
-  document.getElementById("console-img").style.backgroundImage = 'url("img/man.jpg")';
+  document.getElementById("console-img").style.backgroundImage = 'url("img/map.png")';
 
   });
   $('#pills-movie-tab').click(function(){
     // alert('clik');
-  document.getElementById("console-img").style.backgroundImage = 'url("img/kanye.jpg")';
+  document.getElementById("console-img").style.backgroundImage = 'url("img/movie.jpg")';
+  document.getElementById("console-img").style.backgroundPosition = 'right';
 
   });
   $('#pills-hotel-tab').click(function(){
     // alert('clik');
-  document.getElementById("console-img").style.backgroundImage = 'url("img/man.jpg")';
+  document.getElementById("console-img").style.backgroundImage = 'url("img/hotel.jpg")';
 
   });
   $('#pills-dining-tab').click(function(){
     // alert('clik');
-  document.getElementById("console-img").style.backgroundImage = 'url("img/kanye.jpg")';
+  document.getElementById("console-img").style.backgroundImage = 'url("img/dining.jpeg")';
 
   });
   $('#pills-activities-tab').click(function(){
     // alert('clik');
-  document.getElementById("console-img").style.backgroundImage = 'url("img/man.jpg")';
+  document.getElementById("console-img").style.backgroundImage = 'url("img/activity.jpg")';
 
   });
-
 
 
 
